c8cd27edede0c677e68ff258deac0d7a60f8c5d9
henguerz <65286590+henguerz@users.noreply.github.com>
2024-12-04 12:06:57 -0300

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -1,18 +1,5 @@
-dictionaryDescription = "This dictionary contains only one Toki Pona word.",
+dictionaryDescription = "This is the official Q'adrasq dictionary.",
 dictionaryHaveGlosses = false,
 externalDictionary = [
-    {
-        "word": "jan",
-        "uses": [
-            {
-                "hasBadge": false,
-                "badgeText": "ARCHAIC",
-                "type": "NOUN",
-                "explanation": "person, living humanoid being",
-                "example": "Mi lukin e jan ni.",
-                "gloss": "I look (on) human that.",
-                "exampleTranslation": "I'm looking on that man."
-            }
-        ]
-    }
+    {}
 ]
