42a33511768b288bf5b17e8b16b307efc57bca07
henguerz <65286590+henguerz@users.noreply.github.com>
2024-12-04 12:00:27 -0300

    Add files via upload

---
+++ dictionary.js
@@ -0,0 +1,18 @@
+dictionaryDescription = "This dictionary contains only one Toki Pona word.",
+dictionaryHaveGlosses = false,
+externalDictionary = [
+    {
+        "word": "jan",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "ARCHAIC",
+                "type": "NOUN",
+                "explanation": "person, living humanoid being",
+                "example": "Mi lukin e jan ni.",
+                "gloss": "I look (on) human that.",
+                "exampleTranslation": "I'm looking on that man."
+            }
+        ]
+    }
+]
