60323b8d021318774725118cc0e64a24bd303586
henguerz <65286590+henguerz@users.noreply.github.com>
2024-12-04 13:41:09 -0300

    Add files via upload

--- dictionary.js
+++ dictionary.js
@@ -1,3 +1,34 @@
-dictionaryDescription = "This is the official Q'adrasq dictionary.",
+dictionaryDescription = "The official Q'adrasq Dictionary",
 dictionaryHaveGlosses = false,
-externalDictionary = []
+externalDictionary = [
+    {
+        "word": "Q'adrasq",
+        "uses": [
+            {
+                "pronunciation": "kya'drask",
+                "hasBadge": true,
+                "badgeText": "Archaic",
+                "type": "NOUN",
+                "explanation": "Language spoken by the Q'a people.",
+                "example": "Example of this usage (in Imperius)",
+                "exampleTranslation": "Example of this usage (Translation)",
+                "gloss": ""
+            }
+        ]
+    },
+    {
+        "word": "Q'atsuq",
+        "uses": [
+            {
+                "pronunciation": "kya'tsuk",
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "NOUN",
+                "explanation": "The people that speak the Q'a language.",
+                "example": "Example of this usage (in Imperius)",
+                "exampleTranslation": "Example of this usage (Translation)",
+                "gloss": ""
+            }
+        ]
+    }
+] 
